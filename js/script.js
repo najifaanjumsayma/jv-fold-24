0c60903aa9899f0769034fac798c4141be8ac58a
Najifa Anjum Sayma <anjumm3812@gmail.com>
2026-02-25 21:23:33 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// js
+// ex1
+function showDate(){
+
+  document.getElementById('sd').innerHTML=Date();
+}
+// ex2
+function showDate2(){
+   document.getElementById('sdate').innerHTML=Date()
+}
+
+// ex3
+function showT(){
+   document.getElementById('stext').innerHTML="Assalamu Walaikum"; 
+}
+// ex4 
+function bulbOn(){
+   document.getElementById('bulb').src="image/on.gif";
+} 
+function bulbOff(){
+   document.getElementById('bulb').src="image/off.gif";
+}
+// ex5
+function changeStyle(){
+   document.getElementById('cstyle').style.color="green"; 
+}
+// ex6
+function showText(){
+   document.getElementById('text').innerHTML="extraordinary";
+} 
+function hideText(){
+   document.getElementById('text').innerHTML="";
+}
